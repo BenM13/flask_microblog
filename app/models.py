72d6bb98f90d0dea607822b5a6574a7ec89a15f1
Ben Mintz <bmintz@mit.edu>
2018-03-26 16:45:46 -0400

    create registration form

--- app/models.py
+++ app/models.py
@@ -1,17 +1,44 @@
 from datetime import datetime
-from app import db
+from app import db, login
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
 
-class User(db.Model):
+
+# pylint: disable=no-member
+
+class User(UserMixin, db.Model):
+    '''
+    Creates a user database model.
+    '''
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(64), index=True, unique=True)
     password_hash = db.Column(db.String(128))
+    posts = db.relationship('Post', backref='author', lazy='dynamic')
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
 
+    def set_password(self, password):
+        '''
+        Given a user inputed password, this function uses Werkzeug
+        to create a password hash for that user. 
+        '''
+        self.password_hash = generate_password_hash(password)
+    
+    def check_password(self, password):
+        '''
+        Given a user inputed password, this function takes the previously
+        generated password hash and returns True if provided password mathes
+        and False if otherwise.
+        '''
+        return check_password_hash(self.password_hash, password)
 
 class Post(db.Model):
+    '''
+    Creates a post database. Maps the author of the post to the user
+    with user_id as a foreign key.
+    '''
     id = db.Column(db.Integer, primary_key=True)
     body = db.Column(db.String(140))
     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
@@ -19,3 +46,7 @@ class Post(db.Model):
 
     def __repr__(self):
         return '<Post {}>'.format(self.body)
+
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
--- microblog.py
+++ microblog.py
@@ -1,1 +1,11 @@
-from app import app
+from app import app, db
+from app.models import User, Post
+
+@app.shell_context_processor
+def make_shell_context():
+    '''
+    This function is invoked when the flask shell command is called in bash.
+    Gives the ability to work with database entities in the shell without
+    using import statemtns at the beginning of each shell session.
+    '''
+    return {'db': db, 'User': User, 'Post': Post}
