1b8baad9966519620a02a257540c9d2afa028952
Ben Mintz <bmintz@mit.edu>
2018-04-25 10:23:26 -0400

    format date and time using moment.js lib

--- app/__init__.py
+++ app/__init__.py
@@ -5,6 +5,7 @@ from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_mail import Mail
 from flask_bootstrap import Bootstrap
+from flask_moment import Moment
 import logging
 from logging.handlers import SMTPHandler, RotatingFileHandler
 import os
@@ -13,6 +14,7 @@ app = Flask(__name__)
 app.config.from_object(Config)
 
 db = SQLAlchemy(app)
+
 migrate = Migrate(app, db)
 
 login = LoginManager(app)
@@ -21,6 +23,8 @@ login.login_view = 'login'
 mail = Mail(app)
 
 bootstrap = Bootstrap(app)
+
+moment = Moment(app)
 
 from app import routes, models, errors
 
